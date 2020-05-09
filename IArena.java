fe16b31e57433d130d739cbbe450d29829ecb557
DesertFoxHU <40893862+DesertFoxHU@users.noreply.github.com>
2020-05-09 15:59:58 +0200

    Add files via upload

---
+++ IArena.java
@@ -0,0 +1,27 @@
+package me.desertfox.mm;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+/**
+* @author DesertFoxHU
+* @since 2020-05-09
+*/
+public interface IArena {
+
+	public int getMinPlayers();
+	
+	public List<Player> getWaitingPlayers();
+	
+	public void setWaitingPlayers(List<Player> list);
+	
+	public void setElapsedSec(int newValue);
+	
+	public int getElapsedSec();
+	
+	public boolean isAvaible();
+	
+	public void setAvaible(boolean avaible);
+	
+}
